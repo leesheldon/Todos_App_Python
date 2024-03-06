3e852510cf4ae9cd3efb5c2fdf0d7d9e9361e4dd
Sheldon <lhanco@gmail.com>
2024-03-06 23:17:45 +0700

    Implement the Complete and Exit buttons, day 18

--- gui.py
+++ gui.py
@@ -7,9 +7,14 @@ add_button = Sg.Button("Add")
 list_box = Sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=(45, 10))
 edit_button = Sg.Button("Edit")
+complete_button = Sg.Button("Complete")
+exit_button = Sg.Button("Exit")
 
 window = Sg.Window('My To-Do App',
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font=('Helvetica', 16))
 
 while True:
@@ -23,6 +28,7 @@ while True:
 
             # Update added item in list box
             window['todos'].update(values=todos)
+            # Update input box to empty
             window['todo'].update(value='')
 
         case "Edit":
@@ -36,9 +42,25 @@ while True:
 
             # Update edited item in list box
             window['todos'].update(values=todos)
+            # Update input box to empty
             window['todo'].update(value='')
 
-        case 'todos':  # This event occurred once user select item in list box.
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+
+            # Update Todos list in list box after completed item
+            window['todos'].update(values=todos)
+            # Update input box to empty
+            window['todo'].update(value='')
+
+        case "Exit":
+            break
+        case 'todos':
+            # This event occurred once user select item in list box.
 
             # Update user selecting item in list box to input box
             window['todo'].update(value=values['todos'][0])
