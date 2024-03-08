251d6d3fd527868f781e4b78ed322182ea42482f
Sheldon <lhanco@gmail.com>
2024-03-08 10:54:42 +0700

    Implement Complete todo feature, day 19

--- web.py
+++ web.py
@@ -1,43 +1,30 @@
 import streamlit as st
 import functions
 
-
 todos = functions.get_todos()
 
 
 def add_todo():
-    new_todo = st.session_state["input_todo"] + "\n"
-    todos.append(new_todo)
-    functions.write_todos(todos)
+    if st.session_state["input_todo"] != "":
+        new_todo = st.session_state["input_todo"] + "\n"
+        todos.append(new_todo)
+        functions.write_todos(todos)
+
+        st.session_state["input_todo"] = ""
+    else:
+        st.warning("Please enter todo to add!")
 
 
 st.title("My Todo App")
 st.subheader("This is my Todo app.")
 
-for todo in todos:
-    st.checkbox(todo)
-
 st.text_input(label="", placeholder="Add new todo...",
               on_change=add_todo, key="input_todo")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for index, todo in enumerate(todos):
+    chk_box = st.checkbox(todo, key=f"checkbox_{index}")
+    if chk_box:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[f"checkbox_{index}"]
+        st.rerun()
